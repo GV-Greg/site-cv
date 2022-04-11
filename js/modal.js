3d64fec126ac42a799d5bcd22b05f065f94de854
Greg GV <gregvandenbergh@gmail.com>
2022-04-11 12:43:38 +0200

    Implementation of technologies and links in a dynamic way

--- js/modal.js
+++ js/modal.js
@@ -27,18 +27,58 @@ const modalCard = document.querySelector('.modal-card');
 const modalStatut = document.getElementById('modal-statut');
 const modalTitle = document.getElementById('modal-title');
 const modalDescription = document.getElementById('modal-description');
+const modalTechnos = document.getElementById('modal-technologies');
+const modalActions = document.getElementById('modal-actions');
 
 function openModal(project) {
     if(projects.hasOwnProperty(project)) {
         modalStatut.textContent = projects[project].statut;
         modalTitle.textContent = project;
         modalDescription.textContent = projects[project].description;
+        for(let techno in projects[project].technologies) {
+            let tag = document.createElement('div');
+            let newContent = document.createTextNode(techno);
+            tag.appendChild(newContent);
+            tag.classList.add("card-techno");
+            tag.classList.add(projects[project].technologies[techno]);
+            modalTechnos.appendChild(tag);
+        }
+        for(let action in projects[project].actions) {
+            let tag = document.createElement('a');
+            // let newContent = document.createTextNode(action);
+            // tag.appendChild(newContent);
+            tag.href = projects[project].actions[action];
+            tag.target = '_blank';
+            tag.datafront = action;
+            tag.databack = 'visit';
+            tag.classList.add("card-action");
+            let tab = action.split(' ');
+            if(tab.includes('github')) {
+                tag.classList.add('github');
+                tag.innerHTML = '<ion-icon name="logo-github"></ion-icon>' + '<span class="action-text">' + action + '</span>';
+            } else if(tab.includes('site')) {
+                tag.classList.add('site');
+                tag.innerHTML = '<ion-icon name="home"></ion-icon>' + '<span class="action-text">' + action + '</span>';
+            }
+            modalActions.appendChild(tag);
+        }
+        modal.style.display = 'flex';
+        modalCard.style.cssText = 'top:0%; opacity:1; transform:translateY(0)';
+    } else {
+        alert("Pas d'info sur ce projet.")
     }
-    modal.style.display = 'flex';
-    modalCard.style.cssText = 'top:0%; opacity:1; transform:translateY(0)';
+
 }
 
 function closeModal() {
+    let technos = document.getElementsByClassName('card-techno');
+    while(technos.length > 0){
+        modalTechnos.removeChild(modalTechnos.firstElementChild);
+    }
+    let actions = document.getElementsByClassName('card-action');
+    while(actions.length > 0){
+        modalActions.removeChild(modalActions.firstElementChild);
+    }
     modal.style.display = 'none';
     modalCard.style.cssText = 'top:-100%; opacity:1; transform:translateY(-100%)';
 }
